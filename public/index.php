43cd06dd3de44d96779806a013e59b9b903bf7a2
AidarIlyasov <ai.ilyasov@yandex.ru>
2020-03-10 19:02:15 +0300

    Finished with configuration file

---
+++ public/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<link rel="stylesheet" type="text/css" href="/style.css">
+</head>
+<body>
+	<?php
+			echo "asdasdasdads";
+	 	?>
+	<img src="icons/scroll-up.jpg">
+	<script type="text/javascript" src="/test.js"></script>
+</body>
+</html>
